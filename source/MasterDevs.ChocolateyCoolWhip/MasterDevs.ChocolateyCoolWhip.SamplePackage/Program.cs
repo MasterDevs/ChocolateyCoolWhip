08346415536dd80ed50f0d25e065dea66c22c37b
Josh <josh.quintus@gmail.com>
2016-01-30 15:21:38 -0500

    Add a sample project for testing the package.

---
+++ source/MasterDevs.ChocolateyCoolWhip/MasterDevs.ChocolateyCoolWhip.SamplePackage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasterDevs.ChocolateyCoolWhip.SamplePackage
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
