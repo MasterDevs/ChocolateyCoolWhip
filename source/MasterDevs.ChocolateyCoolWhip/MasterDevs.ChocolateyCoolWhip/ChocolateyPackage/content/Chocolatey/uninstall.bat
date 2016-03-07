ae5bfba42e7108e6cf4c5a3844b647317ef9c483
Josh <josh.quintus@gmail.com>
2016-03-06 22:53:10 -0500

    Add comments to uninstall.bat.

--- source/MasterDevs.ChocolateyCoolWhip/MasterDevs.ChocolateyCoolWhip/ChocolateyPackage/content/Chocolatey/uninstall.bat
+++ source/MasterDevs.ChocolateyCoolWhip/MasterDevs.ChocolateyCoolWhip/ChocolateyPackage/content/Chocolatey/uninstall.bat
@@ -1,1 +1,10 @@
-cuninst -y TidyJson.portable
+@ECHO OFF
+REM ********************************************************************************
+REM This script is for debugging chocolatey packaging issues.  it will uninstall 
+REM the existing chocolatey package.  It can be used before or after (most times 
+REM both) the install.bat
+REM
+REM Usage:  uninstall.bat
+REM ********************************************************************************
+
+cuninst -y {{GITHUB_PROJECT_NAME}}.portable
